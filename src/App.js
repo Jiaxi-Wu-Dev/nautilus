2b9e4362397220d95f9b32cce43e9d50eef4edc1
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-28 19:56:58 -0400

    img loaded

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import './App.css';
-import Frontpage from './components/Frontpage'
-
+import React from "react";
+import "./App.css";
+import Frontpage from "./components/Frontpage";
 
 function App() {
   return (
--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // need to import image
-import Background from '/Users/dmac/Desktop/PerScholas/hw/nautilus_mockup/nautilus/src/assets/matches.png'
+import Background from "/Users/dmac/Desktop/PerScholas/hw/nautilus_mockup/nautilus/src/assets/matches.png";
 
 class Frontpage extends Component {
-    state = {  }
-    render() { 
-        return ( <div>
-            <img src={Background} alt="" />
-            </div> );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <div className="matches">
+        <img className="matches" src={Background} width="100%" alt="" />
+          <h3>ISSUE 079</h3>
+          <h1>Catalysts</h1>
+          <p>Agents of Change</p>
+          <p>Read More</p>
+          <button className="issueButton"> See FULL ISSUE </button>
+        </div>
+      </div>
+    );
+  } 
 }
- 
+
 export default Frontpage;
-
-
