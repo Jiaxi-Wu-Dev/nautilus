09e13276658810b62d9f196c8ddf7640e284eb75
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-28 21:37:10 -0400

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-class  extends Component {
+class Sidebar extends Component {
     state = {  }
     render() { 
         return (  );
     }
 }
  
-export default ;
+export default Sidebar;
