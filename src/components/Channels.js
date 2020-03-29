c943d4ee1b840fcc26deac867d7fd562b17ef3e1
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-28 23:59:22 -0400

    channel pics

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Sidebar from "./Sidebar";
 
 class Channels extends Component {
-    state = {  }
-    render() { 
-        return ( <div>
+  state = {};
+  render() {
+    return (
+      <div>
         <div className="heading">
-        <h1> Channels </h1>
-        <h2> TUNE IN TO NAUTILUS DEEP DIVE PORTALS </h2>
-
+          <h1> Channels </h1>
+          <h2> TUNE IN TO NAUTILUS DEEP DIVE PORTALS </h2>
         </div>
-        </div> );
-    }
+
+        <img
+          src="/Users/dmac/Desktop/PerScholas/hw/nautilus_mockup/nautilus/src/assets/bio_and_beyond.jpeg"
+          alt=""
+        />
+        <img
+          src="/Users/dmac/Desktop/PerScholas/hw/nautilus_mockup/nautilus/src/assets/wise_sci_and_engineering.png"
+          alt=""
+        />
+        <img
+          src="/Users/dmac/Desktop/PerScholas/hw/nautilus_mockup/nautilus/src/assets/abstractions.jpeg"
+          alt=""
+        />
+      </div>
+    );
+  }
 }
- 
+
 export default Channels;
