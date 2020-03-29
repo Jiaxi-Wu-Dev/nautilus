9f3879eed78136870c4de7c569be051a118b1cfd
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-28 23:52:41 -0400

    channels section started

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Frontpage from "./components/Frontpage";
 import Midpage from "./components/Midpage"
+import Channels from "./components/Channels"
 
 function App() {
   return (
     <div>
       <Frontpage />
       <Midpage />
+      <Channels />
     </div>
   );
 }
---
+++ src/components/Channels.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Channels extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+        <div className="heading">
+        <h1> Channels </h1>
+        <h2> TUNE IN TO NAUTILUS DEEP DIVE PORTALS </h2>
+
+        </div>
+        </div> );
+    }
+}
+ 
+export default Channels;
